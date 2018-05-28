efa50dae07ab49a1298373edf7a6a8e2555b32b7
andreicampigotto <andreicampigotto@gmail.com>
2018-05-28 01:54:15 -0300

    cardapio

---
+++ TrabalhoAndrei/src/trabalhoandrei/TrabalhoAndrei.java
@@ -0,0 +1,38 @@
+
+package trabalhoandrei;
+import console.Console;
+
+public class TrabalhoAndrei {
+
+    public static void main(String[] args) {
+        /*Enunciado: faça um programa para implementar um controle de caixa de uma lanchonete. 
+        Monte um cardápio de produtos com, no mínimo, 3 produtos e mostre o cardápio para o operador do sistema a 
+        cada venda.
+        O operador poderá escolher os produtos que fazem parte de cada venda, 
+        calcular quanto deverá ser pago a cada venda e mostrar o valor que deve ser pago pelo cliente.
+        No final, o programa deverá informar o valor total das vendas da lanchonete.
+        Lembre-se: a lanchonete pode fazer N vendas e cada venda pode ter N produtos.*/
+        
+        
+        Console.writeLn("      Cardápio lanchonete do seu zé " + 
+                     "\n ---------------------------------------------- " + 
+                     "\n|  1  |        PÃO COM BOLINHO          | 8,50 |" +
+                     "\n|----------------------------------------------|" +
+                     "\n|  2  |      FATIA BOLO DE CENOURA      | 4,25 | " + 
+                     "\n|----------------------------------------------|" +
+                     "\n|  3  |             CAFÉ                | 3,75 |" + 
+                     "\n|----------------------------------------------|" +
+                     "\n|  4  |        SUCO DE LARANJA          | 4,00 |" +
+                     "\n|----------------------------------------------|" +
+                     "\n|  5  |         PÃO DE QUEIJO           | 0,75 |" +
+                     "\n|----------------------------------------------|" +
+                     "\n|  6  |             CUCA                | 2,15 |" + 
+                     "\n|----------------------------------------------|" +
+                     "\n|  7  |            TOTAL                       |" +
+                     "\n ---------------------------------------------- ");
+        
+        
+        
+    }
+    
+}
